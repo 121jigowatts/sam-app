f2f999f2998b921b4c196533867eb82ed8d4ad2d
soil <sh.yoshida@live.jp>
2021-09-18 23:38:13 +0900

    Slack notification (#1)
    * simple slack notification
    
    * update README.md

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,42 +1,35 @@
 import json
+import os
+import requests
 
-# import requests
-
+SLACK_WEBHOOK_URL = os.environ['SLACK_WEBHOOK_URL']
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    print(SLACK_WEBHOOK_URL)
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    payload = {
+        'attachments': [
+            {
+                'color': '#00a1c6',
+                'pretext': "test",
+                'text': "It's works!!"
+            }
+        ]
+    }
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+    try:
+        response = requests.post(SLACK_WEBHOOK_URL, data=json.dumps(payload))
+    except requests.RequestException as e:
+        # Send some context about this error to Lambda Logs
+        print(e)
 
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+        raise e
+    else:
+        print(response.status_code)
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
+            "message": "success",
         }),
     }
